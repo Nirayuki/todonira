2e32b02b60149c31aed8cdfc601af9873eb82df9
Aron <aronkerk8@gmail.com>
2023-07-31 21:50:09 -0300

    Loading in themecontext

--- src/theme/ThemeContext.jsx
+++ src/theme/ThemeContext.jsx
@@ -1,24 +1,32 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { lightTheme, darkTheme } from "./theme";
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 import usePersistedState from '../components/usePersistedState';
 
-
 export const ThemeContext = createContext();
 
-export const ThemeProvider = ({children}) => {
-    const [theme, setTheme] = usePersistedState('themeTodo', 'light');
+export const ThemeProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [theme, setTheme] = usePersistedState('themeTodo', 'light');
 
+  const toggleTheme = () => {
+    setTheme(prevTheme => (prevTheme === 'dark' ? 'light' : 'dark'));
+  };
 
-    const toggleTheme = () => {
-        setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
-    };
+  // Após a restauração do tema, definimos o estado de carregamento como falso.
+  useEffect(() => {
+    setLoading(false);
+  }, []);
 
-    return (
-        <ThemeContext.Provider value={{ theme, toggleTheme }}>
-          <StyledThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
-            {children}
-          </StyledThemeProvider>
-        </ThemeContext.Provider>
-      );
-}
+  if (loading) {
+    return null; // Ou renderize um componente de carregamento ou simplesmente não renderize nada.
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <StyledThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
+        {children}
+      </StyledThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
